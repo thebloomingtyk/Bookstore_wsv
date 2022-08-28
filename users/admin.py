341cddd892692dd92b22fc5a1df102f7e3dce02b
thebloomingtyk <tykdbk@gmail.com>
2022-08-28 23:59:04 +0300

    ch3

---
+++ users/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from . import forms
+
+CustomUser = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    add_form = forms.CustomUserCreationForm
+    form = forms.CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username']
+    
+admin.site.register(CustomUser, CustomUserAdmin)
