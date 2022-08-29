4042d117c6668de34a93210de2f633255961bb63
thebloomingtyk <tykdbk@gmail.com>
2022-08-30 00:11:30 +0300

    ch6 crispy needs modified

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+
+    'crispy_forms',
 ]
+
+# CRISPY_TEMPLATE_PACK = 'boostrap4'
+# CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
@@ -125,6 +131,15 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 LOGIN_REDIRECT_URL = 'home'
 
