ec85c00352a9a467afa4c61701304774e922a5f7
thebloomingtyk <tykdbk@gmail.com>
2022-08-30 23:09:22 +0300

    ch8

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+# from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,11 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pnijttra_qd2sxk6!6=epx4=^=cth=q=*b@_fr7$yy4g9+a*@&'
+# SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = config('DEBUG', cast=bool)
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = int(os.environ.get('DEBUG', default=0))
 ALLOWED_HOSTS = []
 
 # Application definition
@@ -45,7 +49,6 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
 ]
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
