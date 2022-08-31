b76bd52846497314601f5f5d97ecbd3e4c512998
thebloomingtyk <tykdbk@gmail.com>
2022-09-01 02:20:28 +0300

    ch11

--- books/admin.py
+++ books/admin.py
@@ -4,9 +4,15 @@ from books import models
 
 
 # Register your models here.
+class ReviewInline(admin.TabularInline):
+    model = models.Review
+
 
 class BookAdmin(admin.ModelAdmin):
+    inlines = [
+        ReviewInline,
+    ]
     list_display = ("title", "author", "price",)
 
 
-admin.site.register(models.Book)
+admin.site.register(models.Book, BookAdmin)
--- books/tests.py
+++ books/tests.py
@@ -1,18 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from books.models import Book
+from books.models import Book, Review
 
 
 # Create your tests here.
 
 
 class BookTests(TestCase):
+
     def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username = 'reviewuser',
+            email='reviewuser@email.com',
+            password='testpass123'
+        )
+
         self.book = Book.objects.create(
             title='Harry Potter',
             author='JK Rowling',
             price='25.00',
+        )
+        self.review = Review.objects.create(  # new
+            book=self.book,
+            author=self.user,
+            review='An excellent review',
         )
 
     def test_book_listing(self):
@@ -32,4 +45,9 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Harry Potter')
+        self.assertContains(response, 'An excellent review')  # new
         self.assertTemplateUsed(response, 'books/book_detail.html')
+
+
+
+
