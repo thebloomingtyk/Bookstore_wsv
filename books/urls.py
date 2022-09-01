c5dc7cef3448b405cbdf64e9f3c6e481521fe7d0
thebloomingtyk <tykdbk@gmail.com>
2022-09-01 15:27:03 +0300

    ch15

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,5 @@ from books import views
 urlpatterns = [
     path('', views.BookListView.as_view(), name='book_list'),
     path('<uuid:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    path('search/', views.SearchResultsListView.as_view(), name='search_results'),
 ]
