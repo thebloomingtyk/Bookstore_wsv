e084efd89d971ad4024e44816d04d7789298025c
thebloomingtyk <tykdbk@gmail.com>
2022-09-01 03:27:03 +0300

    ch12

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -130,6 +130,10 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
