6363a8c80bc3be64e49dcb55a285be5abfa80e5b
thebloomingtyk <tykdbk@gmail.com>
2022-09-01 19:03:28 +0300

    ch17

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = int(os.environ.get('DEBUG', default=0))
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -200,3 +201,16 @@ STRIPE_TEST_SECRET_KEY = os.environ.get('STRIPE_TEST_SECRET_KEY')
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 
 INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
+
+ENVIRONMENT = os.environ.get('ENVIRONMENT', default='development')
+
+if ENVIRONMENT == 'production':
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = 'DENY'
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
+                  path('anything-but-admin/', admin.site.urls),
                   path('', include('pages.urls')),
                   path('books/', include('books.urls')),
                   path('orders/', include('orders.urls')),
